094b990c59e0082e503b68f708787f3238fb558b
janainascal <janacal@gmail.com>
2018-06-24 19:51:20 -0300

    Adicionando segunda via e viualizar faturas (com id ou lista todas)

--- web2py/applications/ecommerce/controllers/plugin_iugu.py
+++ web2py/applications/ecommerce/controllers/plugin_iugu.py
@@ -10,7 +10,7 @@ def gerar_fatura():
 
     if bill_form.process().accepted:
         #session.resp = iugu.gerar_fatura(bill_form.vars)
-        redirect('visualizar_fatura/' + str(bill_form.vars.id))
+        redirect(URL('visualizar_fatura',args=[bill_form.vars.id]))
 
     elif bill_form.errors:
         session.flash = "Errou no cliente!"
@@ -19,46 +19,17 @@ def gerar_fatura():
     return dict(form = bill_form)
 
 
-def visualizar_fatura():
-    r = request.vars
-    return dict(r=r)
-
-
-def visualizar_fatura2():
-    payer_address = dict(zip_code="29200130", street="R sealworks", number="76", district="Centro", city="Guarapari",
-                         state="ES", country="Brazil", complement="apt")
-    payer = dict(cpf_cnpj="11879553708", name="Jana", phone_prefix="27", phone="9988336552", email="jana@email.com",
-                 address=payer_address)
-
-    form_payer = SQLFORM.dictform(payer)
-    if form_payer.process().accepted:
-        payer.update(form_payer.vars)
-    elif form_payer.errors:
-        session.flash = "Errou no cliente!"
-
-    form_address = SQLFORM.dictform(payer_address)
-    if form_address.process().accepted:
-        payer.update(form_address.vars)
-    elif form_address.errors:
-        session.flash = "Errou no endereço!"
-
-    session.counter = (session.counter or 0) + 1
-    session.list = (session.list or [])
-    item = dict(quantity='1', price_cents='100', description="descricao")
-    form_item = SQLFORM.dictform(item)
-    session.list.append('1')
-    if form_item.process().accepted:
-        print('bbbbbbb')
-        print(form_item)
-        session.list.append(form_item)
-    elif form_item.errors:
-        print('ccccc')
-        session.flash = "Errou no item!"
+def visualizar_faturas():
+    if len(request.args) > 0:
+        bills =[]
+        for x in request.args:
+            bills.append(iugu.get_bills(x))
     else:
-        print('Primeiro form a aparecer')
+        iugu_bills = iugu.get_invoices()
+        bills = iugu_bills['items']
 
-    due_date = '2018-06-17'
-    email = 'janascal@email.com'
-    return dict(form_payer=form_payer, form_item=form_item, form_address=form_address, intems=session.list,
-                counter=session.counter)
-    # return dict(r=iugu.gerar_fatura(payer=payer, items=list,due_date=due_date, email=email))
+    return dict(bills = bills)
+
+def segunda_via():
+    bill = iugu.get_duplicate(request.args(0))
+    return dict(bill = bill)
