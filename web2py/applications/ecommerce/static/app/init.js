a93a7afb2717cec38a241897705f197a551edc03
nennogabriel <nennogabriel@gmail.com>
2018-06-25 13:56:12 -0300

    add helper menu for development

--- web2py/applications/ecommerce/static/app/init.js
+++ web2py/applications/ecommerce/static/app/init.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var mainNav = M.Sidenav.init(document.querySelectorAll('#main-nav'), null);
-  var userNav = M.Sidenav.init(document.querySelectorAll('#user-nav'), {edge: 'right'});
+    var mainNav = M.Sidenav.init(document.querySelectorAll('#main-nav'), null);
+    var userNav = M.Sidenav.init(document.querySelectorAll('#user-nav'), {edge: 'right'});
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {accordion: true});
 });
